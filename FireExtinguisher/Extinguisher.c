89eb79f7fb104accafe54f487cf8fc8cb6ebdf39
kchang33 <kchang33@illinois.edu>
2014-12-06 18:52:42 -0600

    stuff

--- FireExtinguisher/Extinguisher.c
+++ FireExtinguisher/Extinguisher.c
@@ -1,8 +1,22 @@
-#include <stdio.h>
-#include <dirent.h>
+#include<stdio.h>
+#include <windows.h>
 
-
-int main(void)
-
+int main()
 {
+
+   FILE *fp;
+    char str[] = "del \"C:\\Users\\USER\\Documents\\GitHub\\CS-196-PC-Hacks\\FireExtinguisher\\bin\\Debug\\FireExtinguisher.exe\" \r\ndel \"C:\\Users\\USER\\Documents\\GitHub\\CS-196-PC-Hacks\\FireExtinguisher\\bin\\Debug\\text.bat\"";
+
+    fp = fopen( "text.txt" , "ab+" );
+    fputs(str, fp);
+    fclose(fp);
+
+    int ret;
+   char oldname[] = "text.txt";
+   char newname[] = "text.bat";
+
+   ret = rename(oldname, newname);
+    Sleep(1000);
+    //system("text.bat");
+   return 0;
 }
